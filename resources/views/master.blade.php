0f98fe67855e5acf297bccac8299855c4db26ccc
severuseverus <digo.david.oliveira@gmail.com>
2017-08-09 17:26:00 -0300

    Arquivos js e css compactados e minificados

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -16,10 +16,7 @@
     
     <!-- Custom Fonts -->
     <link href="{{ asset('/vendor/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <link href="{{ asset('/fonts/fonts.css') }}" rel="stylesheet" type="text/css">
     
     <!-- Theme CSS -->
     <link href="{{ asset('/css/agency.min.css') }}" rel="stylesheet">
@@ -27,8 +24,8 @@
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
+    <script src="{{ asset('/vendor/html5shiv/html5shiv.js') }}"></script>
+    <script src="{{ asset('/vendor/respond/respond.min.js') }}"></script>
     <![endif]-->
 
 </head>
@@ -394,7 +391,7 @@
 
 <script src="{{ asset('/vendor/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+<script src="{{ asset('/vendor/jquery-easing/1.3/jquery.easing.min.js') }}"></script>
 <script src="{{ asset('/js/jqBootstrapValidation.js') }}"></script>
 <script src="{{ asset('/js/contact_me.js') }}"></script>
 <script src="{{ asset('/js/agency.min.js') }}"></script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,26 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js([
+    'resources/assets/js/jquery/jquery.js',
+    'resources/assets/js/bootstrap/js/bootstrap.js',
+    'resources/assets/js/html5shiv/html5shiv.js',
+    'resources/assets/js/respond/respond.min.js',
+    'resources/assets/js/jquery-easing/1.3/jquery.easing.min.js',
+    'resources/assets/js/jqBootstrapValidation.js',
+    'resources/assets/js/contact_me.js',
+    'resources/assets/js/agency.min.js',
+    'resources/assets/js/app.js'
+], 'public/js/app.js')
+    .styles([
+       'public/vendor/bootstrap/css/bootstrap.min.css',
+       'public/vendor/font-awesome/css/font-awesome.min.css',
+       'public/css/agency.min.css',
+       'resources/assets/sass/app.scss'
+    ], 'public/css/app.css')
+    .styles([
+        'public/fonts/vendor/googleapis/droid-serif.css',
+        'public/fonts/vendor/googleapis/kaushan-script.css',
+        'public/fonts/vendor/googleapis/montserrat-400-700.css',
+        'public/fonts/vendor/googleapis/roboto-slab.css',
+    ], 'public/fonts/fonts.css');
