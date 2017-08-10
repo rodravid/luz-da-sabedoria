9363c0dd8e4f61ee94956061aa87870e13b51bf7
higorpalharini <higorpalharini@outlook.com>
2017-08-10 00:52:24 -0300

    Alterando Título

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -130,7 +130,7 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading">Alguns de nossos projetos</h2>
-                <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                <h3 class="section-subheading text-muted">Conheça um pouco mais sobre os nossos projetos.</h3>
             </div>
         </div>
         <div class="row">
